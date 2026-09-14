92253dc67fd443e005eca5932149a8b2db53173f
Polnfuel <polnfuel@gmail.com>
2026-09-14 20:15:31 +0300

    Revert deallocation of tokens and instructions because it allows leaks when parser returns an error.

--- src/Assembler.zig
+++ src/Assembler.zig
@@ -63,8 +63,6 @@ fn lexicalAnalyzis(self: *Assembler) AssemblerError!void {
 fn syntaxAnalyzis(self: *Assembler) AssemblerError!void {
     for (self.comp_units.items) |unit| {
         var parser = Parser.init(unit.program);
-        defer unit.program.tokens.deinit(utils.alloc);
-
         try parser.parseTokens();
 
         if (!unit.program.flags.has_code and !unit.program.flags.has_data and !unit.program.flags.has_bss) {
@@ -82,8 +80,6 @@ fn codegenPrograms(self: *Assembler) AssemblerError!void {
         if (unit.program.flags.has_code) {
             var codegen = Codegen.init(unit.program);
             defer codegen.deinit();
-            defer unit.program.code_block.deinitInstructions();
-
             try codegen.generateCode();
         }
         // unit.program.printSymbolTable();
